b45fe691cbb55b8273e80574d35e1fb4806a0c7f
Bryan Larson <b32147@gmail.com>
2019-02-08 11:39:13 -0500

    Added some tests

--- pypitest/tests/test_test.py
+++ pypitest/tests/test_test.py
@@ -2,5 +2,8 @@ from unittest import TestCase
 
 
 class TestTest(TestCase):
-    def test_is_string(self):
+    def test_is_true(self):
         self.assertTrue(True)
+
+    def test_is_false(self):
+        self.assertFalse(False)
